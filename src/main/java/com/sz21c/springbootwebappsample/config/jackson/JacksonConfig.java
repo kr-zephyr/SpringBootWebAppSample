c7cfc86dd17af7eb1eacdfba3a01ce70ece8eb3c
SeungChul Lee <scin21c@gmail.com>
2015-07-03 16:54:30 +0900

    기존 jackson의 ObjectMapper 사용은 ObjectMapperUtil을 이용하여 필요 시마다 새로운 instance를 생성하여 사용하도록 되어 있었으나, 이런 경우 instance 생성 비용이 과도함.
    따라서 Singletone bean으로 관리하여 1번만 생성된 후 사용될 수 있도록 개선.

---
+++ src/main/java/com/sz21c/springbootwebappsample/config/jackson/JacksonConfig.java
@@ -0,0 +1,22 @@
+package com.sz21c.springbootwebappsample.config.jackson;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by zephyr on 15. 7. 3..
+ */
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        //** 받아들이려는 Model보다 큰 데이터 구조가 왔을 때, Model에 없는 필드 무시
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        return objectMapper;
+    }
+}
