197d352c9d30927c50f3a1a821a97d29ed2a86c6
kr-zephyr <scin21c@gmail.com>
2016-05-16 11:13:47 +0900

    SpringBoot App의 http port를 properties에서 설정할 수 있도록 변경

--- src/main/java/com/sz21c/springbootwebappsample/config/spring/CustomizationConfig.java
+++ src/main/java/com/sz21c/springbootwebappsample/config/spring/CustomizationConfig.java
@@ -1,13 +1,17 @@
 package com.sz21c.springbootwebappsample.config.spring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomizationConfig {
 
+    @Value("${server.port}")
+    private Integer port;
+
     public void customize(ConfigurableEmbeddedServletContainer container) {
-        container.setPort(8080);
+        container.setPort(port);
     }
 
 }
