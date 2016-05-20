3df69ed5aa597f6622e73e44729630c60a9846dc
kr-zephyr <scin21c@gmail.com>
2016-05-20 16:08:33 +0900

    RDB관련 Test 추가

---
+++ src/test/java/com/sz21c/springbootwebappsample/sampleservice/service/SampleRdbServiceTest.java
@@ -0,0 +1,37 @@
+package com.sz21c.springbootwebappsample.sampleservice.service;
+
+import com.sz21c.springbootwebappsample.SpringBootWebAppSampleApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {SpringBootWebAppSampleApplication.class})
+@WebIntegrationTest
+public class SampleRdbServiceTest {
+
+    @Autowired
+    SampleRdbService sampleRdbService;
+
+    @Test
+    public void testGetSqlTest() throws Exception {
+        String rtn = sampleRdbService.getSqlTest();
+
+        assertNotNull(rtn);
+        assertEquals(rtn, "3");
+    }
+
+    @Test
+    public void testGetSqlTestUsingInterface() throws Exception {
+        String rtn = sampleRdbService.getSqlTestUsingInterface();
+
+        assertNotNull(rtn);
+        assertEquals(rtn, "3");
+    }
+}
