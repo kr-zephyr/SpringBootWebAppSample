6b1ab0b443e8af931905b39f7df9a53087a42b3e
kr-zephyr <scin21c@gmail.com>
2016-06-13 14:14:00 +0900

    MyBatis Setting을 Java Config로 이동

--- src/main/java/com/sz21c/springbootwebappsample/config/mybatis/MyBatisConfig.java
+++ src/main/java/com/sz21c/springbootwebappsample/config/mybatis/MyBatisConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * MyBatis의 sqlmap을 Mapper 기반으로 사용하기 위해 MapperScan을 추가한다.
@@ -30,7 +31,25 @@ public class MyBatisConfig {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource);
         sqlSessionFactoryBean.setConfigLocation(properties.getConfigLocation());
+        sqlSessionFactoryBean.setConfigurationProperties(getMyBatisProperties());
+
         return sqlSessionFactoryBean;
+    }
+
+    /**
+     * MyBatis 내부 속성 설정 정의
+     *
+     * cacheEnabled : MyBatis의 내부 Cache 활성화
+     * lazeLoadingEnabled : 서브 쿼리 등으로 N + 1 쿼리 실행이 발생되는 환경은 getter 등으로 데이터가 호출되는 시점에 쿼리가 실행되도록 설정
+     * defaultExecutorType : SQL 실행 모드를 기본으로 Batch 형태로 실행되도록 설정
+     */
+    private Properties getMyBatisProperties() {
+        Properties properties = new Properties();
+        properties.put("cacheEnabled", true);
+        properties.put("lazyLoadingEnabled", true);
+        properties.put("defaultExecutorType", "BATCH");
+
+        return properties;
     }
 
     @Bean
